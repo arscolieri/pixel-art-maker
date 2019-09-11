36e43a9e9bd2b3cf5d75d786f322bf933bc2630d
Alexandra Scolieri <ars5585@gmail.com>
2019-09-10 22:00:27 -0700

    as of 10pm

--- artMaker.js
+++ artMaker.js
@@ -1,7 +1,7 @@
 window.onload = function(){
+  let currentColor = 'white'
 
-    
-   let container = document.querySelector('#canvas')
+   let container = document.querySelector('#canvas');
 
     for(let i=0; i<400; i++){
         let box = document.createElement('span');
@@ -13,6 +13,16 @@ window.onload = function(){
     container.appendChild(box);
     }
 
-
    document.body.appendChild(container);
-  }
+  
+  let  selector = document.querySelector('#selector');
+
+  let colorChoices= ['red','blue','green','yellow','pink'];
+
+    for (let c=0; c<colorChoices.length; c++){
+       let selectorBox = document.createElement('span');
+        selectorBox.style.backgroundColor = colorChoices[c];
+    }
+   selector.appendChild(selectorBox);
+
+}
